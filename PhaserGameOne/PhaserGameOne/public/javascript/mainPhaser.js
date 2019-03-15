2fc90a579f08589490bb134982516968377789b7
Ana <analiliaf1992@gmail.com>
2019-03-14 21:13:30 -0400

    made interactive button

--- PhaserGameOne/PhaserGameOne/public/javascript/mainPhaser.js
+++ PhaserGameOne/PhaserGameOne/public/javascript/mainPhaser.js
@@ -11,31 +11,77 @@ var config = {
 
 var button;
 var music;
+var cursors;
 
 var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('sky', 'javascript/assets/sky.png');
-    this.load.image('active-button','javascript/assets/RedButton-Hover.png');
 
-    this.load.spritesheet('dude',
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
+    this.load.spritesheet('button',
+        'javascript/assets/button-sheet.png',
+        { frameWidth: 416, frameHeight: 419 }
     );
+ 
 
      this.load.audio('piano', ['javascript/assets/kirby_loop.mp3', 'javascript/assets/kirby_loop.ogg']);
 }
 
 function create() {
+    //add background
     this.add.image(400, 300, 'sky');
-    this.add.image(400, 300, 'active-button');
+
+    //add music
     music = this.sound.add('piano');
     music.loop = true;
     music.play(); 
 
+    //add button sprite
+    button = this.add.sprite(400, 300, 'button');
+    button.setInteractive().on('pointerdown', () => { button.anims.play('on', true); });
+
+    //create cursor object for button input
+    cursors = this.input.keyboard.createCursorKeys();
+    //  Our player animations, turning, walking left and walking right.
+
+    this.anims.create({
+        key: 'off',
+        frames: this.anims.generateFrameNumbers('button', { start: 0, end: 0 }),
+        frameRate: 2,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'on',
+        frames: this.anims.generateFrameNumbers('button', { start: 1, end: 1 }),
+        frameRate: 2,
+        repeat: -1
+    });
+
+
+ 
+
+
+
+
+
+
 }
 
 function update() {
+
+    
+
+    if (cursors.space.isDown) {
+
+        button.anims.play('on', true);
+    }
+    else {
+        button.anims.play('off', true);
+
+    }
+
+
+
 
 }
  
